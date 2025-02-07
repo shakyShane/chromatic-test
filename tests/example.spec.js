278d1f4f22d10bdbb18fa0a051859146de3c3bc3
Shane Osbourne <shane.osbourne8@gmail.com>
2025-02-07 20:17:40 +0000

    another

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@chromatic-com/playwright';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://content-scope-scripts.netlify.app/build/pages/new-tab/?feed=activity&activity=10');
+  await page.goto('https://content-scope-scripts.netlify.app/build/pages/new-tab/?feed=activity&activity=1');
 
   // Expect a title "to contain" a substring.
   await expect(page.locator('[data-entry-point="favorites"]')).toHaveScreenshot('favorites.png')
